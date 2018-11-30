a3be5c9566e4f12613650e32a362d5f2b0293b7f
balaJS <vvbala1995@gmail.com>
2018-12-01 00:03:43 +0530

    new update

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,9 +1,11 @@
+var site = {};
+site.lazyLoad = function() {
+	$('.js-lazy-load').each(function(index,elem){
+		elem.src = $(elem).attr('data-src');
+	});
+}
+
+//calling methods below here
 $(document).ready(function() {
-	var site = {};
-	site.lazyLoad = function() {
-		$('.js-lazy-load').each(function(index,elem){
-			elem.src = $(elem).attr('data-src');
-		});		
-	}
 	site.lazyLoad();
 });
